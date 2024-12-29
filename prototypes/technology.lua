9b7d5efb7d134626449429fd9ccf0292ca66d2a0
snouz <snooz-x@hotmail.com>
2024-12-30 00:48:39 +0100

    2.2.10

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -1,9 +1,21 @@
-table.insert(
-  data.raw["technology"]["oil-processing"].effects,
-  {type = "unlock-recipe",recipe = "flare-stack"})
-table.insert(
-  data.raw["technology"]["oil-processing"].effects,
-  {type = "unlock-recipe",recipe = "vent-stack"})
-table.insert(
-  data.raw["technology"]["advanced-material-processing"].effects,
-  {type = "unlock-recipe",recipe = "electric-incinerator"})
+if data.raw["technology"]["oil-processing"] then
+  table.insert(
+    data.raw["technology"]["oil-processing"].effects,
+    {type = "unlock-recipe",recipe = "flare-stack"})
+  table.insert(
+    data.raw["technology"]["oil-processing"].effects,
+    {type = "unlock-recipe",recipe = "vent-stack"})
+end
+if data.raw["technology"]["angels-oil-processing"] then
+  table.insert(
+    data.raw["technology"]["angels-oil-processing"].effects,
+    {type = "unlock-recipe",recipe = "flare-stack"})
+  table.insert(
+    data.raw["technology"]["angels-oil-processing"].effects,
+    {type = "unlock-recipe",recipe = "vent-stack"})
+end
+if data.raw["technology"]["advanced-material-processing"] then
+  table.insert(
+    data.raw["technology"]["advanced-material-processing"].effects,
+    {type = "unlock-recipe",recipe = "electric-incinerator"})
+end
