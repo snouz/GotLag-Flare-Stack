c46e642bace26a141a1d6ed4e40bc8a07556c145
snouz <snooz-x@hotmail.com>
2024-12-30 00:41:34 +0100

    2.1.2

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -23,9 +23,37 @@ for ki, vi in pairs(data.raw.fluid) do
   })
 end
 
--- generate incineration recipe for every non-fluid item
+-- returns true if string fuel1 represents a higher energy value than string fuel2, eg "8MJ" > "20kJ" is true
+function fuelGreaterThan(fuel1, fuel2)
+  local fuel_suffix_list =
+  {
+    ["J"] = 0,
+    ["kJ"] = 1,
+    ["MJ"] = 2,
+    ["GJ"] = 3,
+    ["TJ"] = 4,
+  }
+  local exp1 = fuel_suffix_list[string.sub(fuel1,string.find(fuel1, "%a+"))]
+  local exp2 = fuel_suffix_list[string.sub(fuel2,string.find(fuel2, "%a+"))]
+  if exp1 == exp2 then
+    local num1 = tonumber(string.sub(fuel1,string.find(fuel1, "%d+")))
+    local num2 = tonumber(string.sub(fuel2,string.find(fuel2, "%d+")))
+    return num1 > num2
+  else
+    return exp1 > exp2
+  end
+end
+
+-- Get fuel value for coal if it exists, else default to vanilla value
+if data.raw.item["coal"] and data.raw.item["coal"].fuel_value then
+  coal_value = data.raw.item["coal"].fuel_value
+else
+  coal_value = "8MJ"
+end
+
 for ki, vi in pairs(data.raw.item) do
-  if not (vi.fuel_category and vi.fuel_category == "chemical") then
+  -- create incineration recipe for any item, and any chemical fuel with less energy than coal
+  if not (vi.fuel_category and vi.fuel_category == "chemical" and not fuelGreaterThan(coal_value, vi.fuel_value)) then
     data:extend({
       {
         type = "recipe",
